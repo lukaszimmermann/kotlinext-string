cbd3e34e57adc79b04f54835aae0a37067cb0eb0
Lukas Zimmermann <luk.zim91@gmail.com>
2018-12-03 16:29:27 +0100

    Add initial functions
    ensureTrailing
    ensureTrailingSlash
    containsWhitespace
    
    Signed-off-by: Lukas Zimmermann <luk.zim91@gmail.com>

---
+++ src/main/kotlin/kotlinext/String.kt
@@ -0,0 +1,31 @@
+package kotlinext
+
+/**
+ * Ensures that this [String] ends with the provided [Char].
+ *
+ * If this [String] already ends with the provided [Char], then the [String] is returned unaltered.
+ * Otherwise, the [Char] is appended and the resulting [String] is returned.
+ *
+ * @param symbol The [Char] which should occur at the end of this [String].
+ * @return The input [String] which is guaranteed to end with the provided [Char].
+ *
+ */
+fun String.ensureTrailing(symbol: Char): String = if (endsWith(symbol)) this else "$this$symbol"
+
+/**
+ * Ensures that this [String] ends with the '/' [Char].
+ *
+ * If this [String] already ends with '/', then the [String] is returned unaltered.
+ * Otherwise, '/' is appended and the resulting [String] is returned.
+ *
+ * @return The input [String] which is guaranteed to end with '/'.
+ *
+ */
+fun String.ensureTrailingSlash(): String = this.ensureTrailing('/')
+
+/**
+ *  Determines whether this [String] contains any whitespace [Char] as defined by the Unicode standard.
+ *
+ * @return True if this [String] contains at least one whitespace character, false if not.
+ */
+fun String.containsWhitespace(): Boolean = this.any { it.isWhitespace() }
---
+++ src/test/kotlin/kotlinext/StringTests.kt
@@ -0,0 +1,35 @@
+package kotlinext
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Test
+
+class StringTests {
+
+    @Test
+    fun ensureTrailing() {
+        assertEquals("foo$", "foo$".ensureTrailing('$'))
+        assertEquals("foo$", "foo".ensureTrailing('$'))
+        assertEquals("#", "#".ensureTrailing('#'))
+        assertEquals("#", "".ensureTrailing('#'))
+    }
+
+    @Test
+    fun ensureTrailingSlash() {
+        assertEquals("foo/", "foo/".ensureTrailingSlash())
+        assertEquals("foo/", "foo".ensureTrailingSlash())
+        assertEquals("/", "/".ensureTrailingSlash())
+        assertEquals("/", "".ensureTrailingSlash())
+    }
+
+    @Test
+    fun containsWhitespace() {
+        assertTrue(" ".containsWhitespace())
+        assertTrue(" aldjakfhi".containsWhitespace())
+        assertTrue("aldj akfhi".containsWhitespace())
+        assertTrue("aldjakfhi\t".containsWhitespace())
+        assertFalse("".containsWhitespace())
+        assertFalse("foo".containsWhitespace())
+    }
+}
